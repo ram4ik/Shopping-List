5b558e4456f871dd16e322c7a3ec6e7b1fc5e1a6
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-09-29 13:21:43 +0300

    init

--- Shopping List/ContentView.swift
+++ Shopping List/ContentView.swift
@@ -9,13 +9,36 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List {
+                ShoppingCell()
+            }
+            .navigationTitle("Shopping List")
+        }
+    }
+}
+
+struct ShoppingCell: View {
+    @State private var isSelected = false
+    
+    var body: some View {
+        HStack {
+            Button(action: {
+                isSelected.toggle()
+            }, label: {
+                Image(systemName: isSelected ? "largecircle.fill.circle" : "circle")
+            })
+            .buttonStyle(PlainButtonStyle())
+            
+            Text("Bananen")
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+        ShoppingCell()
+            .previewLayout(.sizeThatFits)
     }
 }

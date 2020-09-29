00f6a2cb041a31b8c08d59fd334f4b9ef2f1fa16
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-09-29 13:06:55 +0300

    Initial Commit

---
+++ Shopping List/Shopping_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Shopping_ListApp.swift
+//  Shopping List
+//
+//  Created by ramil on 29.09.2020.
+//
+
+import SwiftUI
+
+@main
+struct Shopping_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
